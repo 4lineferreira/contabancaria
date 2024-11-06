93753e50c12e556892db8cb0851986c3a2e926ba
4lineferreira <alineferreirasnsd@gmail.com>
2024-11-06 13:39:41 -0300

    Task 2 conta e conta.model

--- conta/Menu.java
+++ conta/Menu.java
@@ -2,19 +2,27 @@ package conta;
 
 import java.util.Scanner;
 import conta.util.Cores;
+import conta.model.Conta;
 
 public class Menu {
 
 	public static void main(String[] args) {
+
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.Visualizar();
+		c1.Sacar(12000.0f);
+		c1.Visualizar();
+		c1.Depositar(5000.0f);
+		c1.Visualizar();
 
 		Scanner scanner = new Scanner(System.in);
 
 		int opcao;
 
 		while (true) {
-			
-			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND 
-							 + "**************************************************************");
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "**************************************************************");
 			System.out.println("                                                              ");
 			System.out.println("                      BANCO DO BRAZIL COM Z                   ");
 			System.out.println("                                                              ");
@@ -39,7 +47,8 @@ public class Menu {
 			opcao = scanner.nextInt();
 
 			if (opcao == 9) {
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Banco do Brazil com Z - Simples, rápido e inovador só que com Z ");
+				System.out.println(
+						Cores.TEXT_WHITE_BOLD + "Banco do Brazil com Z - Simples, rápido e inovador só que com Z ");
 				sobre();
 				scanner.close();
 				System.exit(0);
@@ -47,35 +56,35 @@ public class Menu {
 			}
 			switch (opcao) {
 			case 1:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Criar conta\n\n");
+				System.out.println(Cores.ANSI_BLACK_BACKGROUND + "Criar conta\n\n");
 
 				break;
 			case 2:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todos as contas\n\n");
+				System.out.println(Cores.ANSI_BLACK_BACKGROUND + "Listar todos as contas\n\n");
 
 				break;
 			case 3:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da conta\n\n");
 
 				break;
 			case 4:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da conta \n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da conta \n\n");
 
 				break;
 			case 5:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar a conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a conta\n\n");
 
 				break;
 			case 6:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 
 				break;
 			case 7:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 
 				break;
 			case 8:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Transferência entre contas\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre contas\n\n");
 
 				break;
 			default:
@@ -89,11 +98,11 @@ public class Menu {
 	public static void sobre() {
 		System.out.println("\n**************************************************************");
 		System.out.println("*                                                            *");
-		System.out.println("*                Projeto Desenvolvido por:                   *");
+		System.out.println("*                  Projeto Desenvolvido por:                 *");
 		System.out.println("*         Aline Ferreira - alineferreirasnsd@gmail.com       *");
 		System.out.println("*        https://github.com/4lineferreira/contabancaria      *");
 		System.out.println("*                                                            *");
 		System.out.println("**************************************************************");
 	}
-
+		
 }
---
+++ conta/model/Conta.java
@@ -0,0 +1,98 @@
+package conta.model;
+
+import conta.util.Cores;
+
+public class Conta {
+
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private String titular;
+	private float saldo;
+
+	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
+
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public boolean Sacar(float valor) {
+
+		if (this.getSaldo() < valor) {
+			System.out.println("\n Saldo Insuficiente");
+			return false;
+		}
+
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+	}
+
+	public void Depositar(float valor) {
+		this.setSaldo(this.getSaldo() + valor);
+	}
+
+	public void Visualizar() {
+
+		String tipo = "";
+
+		switch (this.tipo) {
+		case 1:
+			tipo = "Conta corrente";
+			break;
+		case 2:
+			tipo = "Conta poupança";
+			break;
+		}
+		System.out.println(Cores.TEXT_RED_BOLD + "\n\n***********************************************************");
+		System.out.println(Cores.TEXT_WHITE_BOLD + "Dados da conta: ");
+		System.out.println(Cores.TEXT_RED_BOLD + "***********************************************************");
+		System.out.println(Cores.TEXT_WHITE_BOLD + "Número da conta: " + this.numero);
+		System.out.println(Cores.TEXT_WHITE_BOLD + "Agência: " + this.agencia);
+		System.out.println(Cores.TEXT_WHITE_BOLD + "Titular: " + this.titular);
+		System.out.println(Cores.TEXT_WHITE_BOLD + "Saldo: " + this.saldo);
+
+	}
+}
